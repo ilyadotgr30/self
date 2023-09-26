d9a5958edb81d7d49e792adbe4b7e7eccfc2fbf7
ilyadotgr30 <ilya.ofc30@gmail.com>
2023-09-26 18:50:46 +0300

    test

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar app color="primary">
+    <v-toolbar-title class="white--text">Моя шапка сайта</v-toolbar-title>
+    <!-- Добавьте другие элементы вашей шапки, например, навигационное меню -->
+  </v-app-bar>
+</template>
+  
+  <script>
+  export default {
+    name: 'MyHeader',
+  }
+  </script>
